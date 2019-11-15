086bceb27fc3373af6845c6e621ae1a4d3b8a242
samz7 <saminc97@gmail.com>
2019-11-16 00:33:44 +0600

    changed the dataset to output 64X64 images and removed generator calling in dataset

--- dataprep.py
+++ dataprep.py
@@ -1,41 +1,45 @@
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, Dataset
+import torch
 from PIL import Image
+import os
 import random
 
 class PaintDataset(Dataset):
-    def __init__(self, generator, transform):
+    def __init__(self, root, transform):
         super().__init__()
-        self.generator = generator
-        self.img_names = os.listdir('images/Sandro_Botticelli')
-        self.labels = [0,1]
+        self.root = root
+        self.img_names = os.listdir(self.root)
+        self.transform = transform
+
+
+    def __len__(self):
+        return len(self.img_names)
 
     def __getitem__(self, idx):
-        label = random.choice(self.labels)
-        label_tensor = torch.tensor([0, 0])
-        if label:
-            img = Image.open(self.img_names[idx])
-            img = transforms(img)
-            label_tensor = label_tensor[label].add_(1)
-            return (img, label_tensor)
-
-        img = self.generator(torch.randn(1, 3, 64, 64))
-        label_tensor = label_tensor[label].add_(1)
+        label_tensor = torch.tensor(1.)
+        img = Image.open(self.root+self.img_names[idx])
+        img = self.transform(img)
         return (img, label_tensor)
 
 
 
 
-train_transform = transforms.Compose([transforms.Resize((172, 172)), 
-                                        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-                                        transforms.ToTensor()
+
+train_transform = transforms.Compose([transforms.Resize((64, 64)), 
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
                                         ])
 
 
 
+image_path = 'images/Sandro_Botticelli/'
 
+dataset = PaintDataset(image_path, train_transform)
+print(len(dataset))
 
+print(dataset[0])
 
 
 
